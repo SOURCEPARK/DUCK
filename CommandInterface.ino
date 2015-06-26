9228344a15bb4e509ca8b591358aac03c18885de
Matthias Barmeier <matthias.barmeier@sourcepark.de>
2015-06-26 18:24:18 +0200

    Kommandointerface implemntiert

--- CommandInterface.ino
+++ CommandInterface.ino
@@ -1,10 +1,7 @@
 String waitForCommand() {
   if (Serial.available() > 0) {
-    char cmd[4];
-    cmd[0] = Serial.read();
-    cmd[1] = Serial.read();
-    cmd[2] = Serial.read();
-    cmd[3] = '\0';
+    String cmd;
+    cmd = Serial.readStringUntil('.');
     return cmd;
   }
   return "A";
--- DUCK.ino
+++ DUCK.ino
@@ -1,21 +1,77 @@
+String cmd[] = {
+  "0203040506NNNNNN",
+  "0203040506NNNNNN",
+  "0203040506NNNNNN",
+  "0203040506NNNNNN",
+  "0203040506020304",
+  "0203040506020304"
+};
+
 void setup() {
+
   Serial.begin(115200);
-   while (!Serial) {
-     delay (10);
+  pinMode(13, OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(7,INPUT);
+  while (!Serial) {
+    delay (10);
   }
-  Serial.println("DUCK Ready\n>");
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(13, LOW);   
+  Serial.print("DUCK Ready\r\n>");
 }
 
 void loop() {
   String s = waitForCommand();
   if (s != "A") {
     Serial.print("Ejecting row:"); 
-    Serial.print(s[1]); 
+    char tmp[] = {
+      '\0','\0','\0'        };
+    tmp[0] = s[1];
+    int row = atoi(tmp);
+    Serial.print(row); 
     Serial.print(" tube:"); 
-    Serial.println(s[2]);
+    int col = atoi(&s[2]);
+    Serial.println(col);
+
+    if (row>5 || row<0 || col>7||col<0) {
+      Serial.println("RANGEERROR");
+    }
+    else {
+      String line = cmd[row]; 
+
+      tmp[0] = line[col*2];
+      tmp[1] = line[col*2+1];
+      if (String(tmp).compareTo("NN")!=0) {
+        Serial.println(tmp);
+        int pin = atoi(tmp);
+
+        Serial.print(" signaling pin: ");
+        Serial.println(pin);
+        Serial.println(line);
+        Serial.println(line[col]);
+        digitalWrite(13, HIGH);
+
+        digitalWrite(pin, LOW);
+        delay(2000);    
+        digitalWrite(pin, HIGH);
+        digitalWrite(13, LOW);    
+      }
+      else {
+        Serial.println("INDEXERROR");
+      }
+    }
   }
-  delay(1000);
+  delay(10);
 }
+
+
 
 
 
